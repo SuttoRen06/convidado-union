812680f80e553c14378d9e8f30a28bff26830038
unknown <renansutto01@gmail.com>
2020-09-21 20:50:32 -0300

    react

---
+++ build/precache-manifest.4c6bff211e101ddda0915b9ab76bf20d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adad406c5e3115be7aa89fd446173fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af62d518649ba28d533",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "35c5e5dafb50b102be51",
+    "url": "/static/css/main.ec81f005.chunk.css"
+  },
+  {
+    "revision": "3af62d518649ba28d533",
+    "url": "/static/js/2.d92dcc84.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d92dcc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c5e5dafb50b102be51",
+    "url": "/static/js/main.1826b2d0.chunk.js"
+  },
+  {
+    "revision": "10fe43946ffc5cc9cc20",
+    "url": "/static/js/runtime-main.6e847328.js"
+  }
+]);
